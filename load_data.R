45efcc6b3e724f914dc7f8c2fed911aa193c832f
Erica Ram <eram1294@gmail.com>
2016-07-12 10:13:23 -0400

    added neighbourhoods and saving to an RData file to the load_data script, changed gitignore

--- load_data.R
+++ load_data.R
@@ -5,3 +5,6 @@ library(readr)
 reviews <- read_csv("reviews.csv", na='\\N')
 listings <- read_csv("listings.csv", na='\\N')
 calendar <- read_csv("calendar.csv", na='\\N')
+neighbourhoods <- read_csv("neighbourhoods.csv", na='\\N')
+
+save(listings, calendar, neighbourhoods, reviews, file = "airbnb.RData")
