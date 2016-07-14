401aa76f8f5a4d275097c0abd5abc7e97053de98
Erica Ram <eram1294@gmail.com>
2016-07-14 14:21:32 -0400

    more maps, script created to pull older listings

---
+++ raw_data/pull_old_listings.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 
+# description:
+#   fetches airbnb listings data from insideairbnb.com from September 2015 to June 2016
+#
+# usage: ./pull_old_listings.sh
+
+
+dates=("2015-09-01" "2015-10-01" "2015-11-01" "2015-11-20" "2015-12-02" "2016-01-01" "2016-02-02" "2016-04-03" "2016-05-02" "2016-06-02") 
+
+for d in ${dates[@]}
+do
+  wget "http://data.insideairbnb.com/united-states/ny/new-york-city/"$d"/data/listings.csv.gz"
+  gzip -d listings.csv.gz
+  mv listings.csv $d-listings.csv
+done
