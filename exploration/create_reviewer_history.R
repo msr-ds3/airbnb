4acb14e72803ead8339ae0f78696ccb3019e42d9
Erica Ram <eram1294@gmail.com>
2016-07-22 17:18:47 -0400

    made changes to reviewer history data frame

--- exploration/create_reviewer_history.R
+++ exploration/create_reviewer_history.R
@@ -22,8 +22,8 @@ save(reviews, file = "ten_cities_reviews.RData")
 load("ten_cities_reviews.RData")
 
 # function to generate dataframe with each reviewer's first month, last month,
-# number of reviews with a specified time period, number of reviews before 2016,
-# number of reviews from all time, name, and review text
+# number of reviews before 2016, number of reviews from all time, name, 
+# and the first, last, total, and review text in a specified time period
 # params: df - data frame
 #         start_month - starting month for num_within_time_period column
 #         end_month - ending month for num_within_time_period column
@@ -38,18 +38,21 @@ get_reviewer_data <- function(df, start_month = FALSE, end_month = "2015-12-31",
   tmp_1 <- group_by(df, reviewer_id) %>%
     arrange(date) %>%
     summarize(first_month = first(date),
+              first_month_num = month(first(date)),
               last_month = last(date),
-              num_within_time_period = sum(date >= as.Date(start_month, "%Y-%m-%d") 
-                                           & date <= as.Date(end_month, "%Y-%m-%d")),
-              num_before_2016 = sum(date < as.Date("2016-01-01", "%Y-%m-%d")),
+              last_month_num = month(last(date)),
               num_in_2016 = sum(date >= as.Date("2016-01-01", "%Y-%m-%d") 
                                 & date <= as.Date(end_in_2016, "%Y-%m-%d")),
               all_time_reviews = n(),
               name = first(reviewer_name))
   
   tmp_2 <- group_by(df, reviewer_id) %>%
-    filter(date <= as.Date(end_in_2016, "%Y-%m-%d")) %>%
-    summarize(text = paste(comments, collapse = "|"))
+    filter(date >= as.Date(start_month, "%Y-%m-%d") 
+           & date <= as.Date(end_month, "%Y-%m-%d")) %>%
+    summarize(text_within_time_period = paste(comments, collapse = "|"),
+              num_within_time_period = n(),
+              first_within_time_period = first(date),
+              last_within_time_period = last(date))
   
   inner_join(tmp_1, tmp_2, by = "reviewer_id")
 }
@@ -60,6 +63,9 @@ get_reviewer_data <- function(df, start_month = FALSE, end_month = "2015-12-31",
 
 # starting with January 2015
 reviewer_data <- get_reviewer_data(reviews, "2015-01-01", "2015-12-31")
+
+# write to csv
+write_csv(reviewer_data, path = "reviewer_data.csv")
 
 # saving reviews and resulting data
 save(reviews, reviewer_data, file = "reviewer_data.RData")
