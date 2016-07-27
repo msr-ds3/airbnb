debae61c75372a0800ac90383fc8aee346f76619
Jacqueline curran <jcurran.student@manhattan.edu>
2016-07-27 16:35:35 -0400

    added in verifications and ammenities

--- predictions/tree_jackie.R
+++ predictions/tree_jackie.R
@@ -582,7 +582,7 @@ jan_tree <- rpart(has_reviews_2016 ~ host_listings_count + host_duration +
                             num_as_of_2015 + num_reviews_in_2015 + has_reviews_2015 +
                             first_review_month_2015 + last_review_month_2015 + 
                             last_rating + mean_price + max_price + min_price + 
-                            room_type + is_multilisting, 
+                            room_type + is_multilisting , 
                           data = listings_history_train_jan, 
                           control = rpart.control(maxdepth = 5))
 printcp(jan_tree)
@@ -605,4 +605,109 @@ abline(a=0, b= 1)
 #find the area under the roc curve
 jan_auc <- performance(jan_ROCR, measure = "auc")
 jan_auc@y.values
+#0.9115
+
+#added in all ammenities
+jan_tree_amen <- rpart(has_reviews_2016 ~ TV + Internet + Wireless.Internet +
+                         Air.Conditioning + Kitchen + Heating + Family.Kid.Friendly + 
+                         Smoke.Detector + Carbon.Monoxide.Detector + Essentials +
+                         Shampoo + Cable.TV + Free.Parking.on.Premises + Breakfast +
+                         Pets.live.on.this.property + Dog.s. + First.Aid.Kit + 
+                         Buzzer.Wireless.Intercom + Washer + Dryer + Pets.Allowed + 
+                         Gym + Safety.Card + Fire.Extinguisher + Wheelchair.Accessible +
+                         Cat.s. + Indoor.Fireplace + Suitable.for.Events + Doorman + 
+                         Hot.Tub + Elevator.in.Building + Pool + Smoking.Allowed + 
+                         Other.pet.s. + Washer...Dryer + Lock.on.Bedroom.Door + 
+                         X24.Hour.Check.in + Hangers + Hair.Dryer + Iron + 
+                         Laptop.Friendly.Workspace + 
+                         translation.missing..en.hosting_amenity_49 + 
+                         translation.missing..en.hosting_amenity_50 + 
+                         host_listings_count + host_duration + 
+                         first_seen_month + last_seen_month + 
+                         listing_recency_2015_weeks + scrap_duration + 
+                         total_occ_2015 + review_recency_2015_weeks + 
+                         is_superhost_2015 + is_superhost_count_2015 + 
+                         first_review_year + last_review_year +
+                         num_as_of_2015 + num_reviews_in_2015 + has_reviews_2015 +
+                         first_review_month_2015 + last_review_month_2015 + 
+                         last_rating + mean_price + max_price + min_price , 
+                  data = listings_history_train_jan, 
+                  control = rpart.control(maxdepth = 5))
+printcp(jan_tree_amen)
+jan_amen_bestcp <- jan_tree_amen$cptable[which.min(jan_tree_amen$cptable[,"xerror"]), "CP"]
+#prune tree using best cp
+jan_amen_tree_pruned <- prune(jan_tree_amen, cp = jan_amen_bestcp)
+
+#plots
+plot(jan_amen_tree_pruned, uniform = TRUE)
+text(jan_amen_tree_pruned, cex = 0.8, use.n = TRUE, xpd = TRUE)
+prp(jan_amen_tree_pruned, faclen = 0, cex = 0.8, extra = 1)
+
+#use the tree_pruned_rf to predict on the test set
+jan_amen_predict <- predict(jan_amen_tree_pruned, listings_history_test_jan)
+jan_amen_ROCR <- prediction(jan_amen_predict, listings_history_test_jan$has_reviews_2016)
+jan_amen_roc_perf <- performance(jan_amen_ROCR, measure = "tpr", x.measure = "fpr")
+#plot the ROC curve
+plot(jan_amen_roc_perf)
+abline(a=0, b= 1)
+#find the area under the roc curve
+jan_amen_auc <- performance(jan_amen_ROCR, measure = "auc")
+jan_amen_auc@y.values
+#0.9115
+
+#look at verification
+
+ 
+
+jan_tree_ver <- rpart(has_reviews_2016 ~ TV + Internet + Wireless.Internet +
+                        Air.Conditioning + Kitchen + Heating + Family.Kid.Friendly + 
+                        Smoke.Detector + Carbon.Monoxide.Detector + Essentials +
+                        Shampoo + Cable.TV + Free.Parking.on.Premises + Breakfast +
+                        Pets.live.on.this.property + Dog.s. + First.Aid.Kit + 
+                        Buzzer.Wireless.Intercom + Washer + Dryer + Pets.Allowed + 
+                        Gym + Safety.Card + Fire.Extinguisher + Wheelchair.Accessible +
+                        Cat.s. + Indoor.Fireplace + Suitable.for.Events + Doorman + 
+                        Hot.Tub + Elevator.in.Building + Pool + Smoking.Allowed + 
+                        Other.pet.s. + Washer...Dryer + Lock.on.Bedroom.Door + 
+                        X24.Hour.Check.in + Hangers + Hair.Dryer + Iron + 
+                        Laptop.Friendly.Workspace + 
+                        translation.missing..en.hosting_amenity_49 + 
+                        translation.missing..en.hosting_amenity_50 + 
+                        host_listings_count + host_duration + 
+                        first_seen_month + last_seen_month + 
+                        listing_recency_2015_weeks + scrap_duration + 
+                        total_occ_2015 + review_recency_2015_weeks + 
+                        is_superhost_2015 + is_superhost_count_2015 + 
+                        first_review_year + last_review_year +
+                        num_as_of_2015 + num_reviews_in_2015 + has_reviews_2015 +
+                        first_review_month_2015 + last_review_month_2015 + 
+                        last_rating + mean_price + max_price + min_price + 
+                        email + phone +
+                        facebook + reviews + kba + google + jumio + sent_id + 
+                        linkedin +
+                        manual_offline + manual_online + weibo + photographer + 
+                        None +
+                        amex + verifications_count, 
+                       data = listings_history_train_jan, 
+                       control = rpart.control(maxdepth = 5))
+printcp(jan_tree_ver)
+jan_ver_bestcp <- jan_tree_ver$cptable[which.min(jan_tree_ver$cptable[,"xerror"]), "CP"]
+#prune tree using best cp
+jan_ver_tree_pruned <- prune(jan_tree_ver, cp = jan_ver_bestcp)
+
+#plots
+plot(jan_ver_tree_pruned, uniform = TRUE)
+text(jan_ver_tree_pruned, cex = 0.8, use.n = TRUE, xpd = TRUE)
+prp(jan_ver_tree_pruned, faclen = 0, cex = 0.8, extra = 1)
+
+#use the tree_pruned_rf to predict on the test set
+jan_ver_predict <- predict(jan_ver_tree_pruned, listings_history_test_jan)
+jan_ver_ROCR <- prediction(jan_ver_predict, listings_history_test_jan$has_reviews_2016)
+jan_ver_roc_perf <- performance(jan_ver_ROCR, measure = "tpr", x.measure = "fpr")
+#plot the ROC curve
+plot(jan_ver_roc_perf)
+abline(a=0, b= 1)
+#find the area under the roc curve
+jan_ver_auc <- performance(jan_ver_ROCR, measure = "auc")
+jan_ver_auc@y.values
 #0.9115
