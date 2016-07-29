5e647d413996e202b10ff3c72bc0bf8dededb0f5
Kaciny Calixte <kacinycalixte@gmail.com>
2016-07-29 09:05:53 -0400

    dealt with NAs in listings-history

--- exploration/historical_exploration/listings_history.R
+++ exploration/historical_exploration/listings_history.R
@@ -177,30 +177,27 @@ rm(first_seen, last_seen, total_occ_2015, first_seen_review, last_seen_review, m
 all_listings_2015_temp <- full_join(all_listings_2015, listings_history, by= "listing_id")
 
 last_record <- filter(all_listings_2015_temp, last_scraped == last_seen)
-last_record <- mutate(last_record, has_reviews_2015 = (number_of_reviews > 0))
+
 
 
 last_rating <- distinct(select(last_record, host_id, listing_id, host_since, review_scores_rating, 
-                               number_of_reviews, has_reviews_2015, host_is_superhost, property_type, room_type))
-names(last_rating)[names(last_rating) == 'number_of_reviews'] <- 'number_of_reviews_last'
+                               host_is_superhost, property_type, room_type))
 names(last_rating)[names(last_rating) == 'review_scores_rating'] <- 'last_rating'
 names(last_rating)[names(last_rating) == 'host_is_superhost'] <- 'is_superhost_2015'
 
 first_record <- filter(all_listings_2015_temp, last_scraped == first_seen)
-first_rating <- distinct(select(first_record, listing_id, number_of_reviews))
-names(first_rating)[names(first_rating) == 'number_of_reviews'] <- 'number_of_reviews_first'
+first_rating <- distinct(select(first_record, listing_id))
 
-last_rating <- full_join(first_rating, last_rating, by= "listing_id") %>% 
-  mutate(num_reviews_2015 = (number_of_reviews_last - number_of_reviews_first))
+last_rating <- full_join(first_rating, last_rating, by= "listing_id")
 
 listings_history <- full_join(listings_history, last_rating, by="listing_id")
 
 rm(last_rating, last_record, all_listings_2015_temp, first_rating, first_record)
 
 
-names(listings_history)[names(listings_history) == 'number_of_reviews_last'] <- 'reviews_total_2015'
+
 listings_history <- listings_history[,c("host_id", "listing_id", "property_type", "room_type","host_since", "first_seen", "last_seen", 
-                                        "first_seen_month", "last_seen_month", "reviews_total_2015", "num_reviews_2015","has_reviews_2015", 
+                                        "first_seen_month", "last_seen_month", 
                                         "first_review", "last_review","last_rating", 
                                         "total_occ_2015", "mean_price", "min_price", "max_price", "is_superhost_2015")]
 
@@ -215,28 +212,23 @@ all_listings_2016_temp <- full_join(all_listings_2016, last_seen_2016, by= "list
 all_listings_2016_temp <- full_join(all_listings_2016_temp, first_seen_2016, by= "listing_id")
 
 last_record_2016 <- filter(all_listings_2016_temp, last_scraped == last_seen_2016)
-last_rating_2016 <- distinct(select(last_record_2016, listing_id, number_of_reviews, host_is_superhost))
-last_rating_2016 <- mutate(last_record_2016, has_reviews_2016 = number_of_reviews > 0)
+last_rating_2016 <- distinct(select(last_record_2016, listing_id, host_is_superhost))
 total_occ_2016 <- all_listings_2016 %>% count(listing_id)
 
-names(last_rating_2016)[names(last_rating_2016) == 'number_of_reviews'] <- 'reviews_total_2016'
 names(last_rating_2016)[names(last_rating_2016) == 'host_is_superhost'] <- 'is_superhost_2016'
 names(total_occ_2016)[names(total_occ_2016) == 'n'] <- 'total_occ_2016'
 
 
 first_record_2016 <- filter(all_listings_2016_temp, last_scraped == first_seen_2016)
-first_rating_2016 <- distinct(select(first_record_2016, listing_id, number_of_reviews))
-names(first_rating_2016)[names(first_rating_2016) == 'number_of_reviews'] <- 'number_of_reviews_first'
+first_rating_2016 <- distinct(select(first_record_2016, listing_id))
 
 
 
-last_rating_2016 <- full_join(first_rating_2016, last_rating_2016, by= "listing_id") %>% 
-  mutate(num_reviews_2016 = (reviews_total_2016 - number_of_reviews_first))
+last_rating_2016 <- full_join(first_rating_2016, last_rating_2016, by= "listing_id")
 
 
 listings_history_2016 <- full_join(total_occ_2016, last_rating_2016, by="listing_id")
-listings_history_2016 <- listings_history_2016[,c("listing_id","reviews_total_2016",
-                                                  "num_reviews_2016","has_reviews_2016","total_occ_2016", "is_superhost_2016")]
+listings_history_2016 <- listings_history_2016[,c("listing_id","total_occ_2016", "is_superhost_2016")]
 
 listings_history <- left_join(listings_history, listings_history_2016, by="listing_id")
 
@@ -286,15 +278,18 @@ listings_history <- listings_history %>% group_by(host_id) %>% mutate(host_listi
 
 
 
-load("../ny_reviews.RData")
+load("ny_reviews.RData")
 
 
 
 listings_history <- left_join(listings_history , ny_reviews, by="listing_id")
-listings_history <- mutate(listings_history, review_recency_2015_weeks = difftime("2016-01-01", last_review_2015, unit = "weeks"), 
+
+
+listings_history <- mutate(listings_history,
                            first_review_month_2015 = month(first_review_2015),
-                           last_review_month_2015 = month(last_review_2015)
-                           )
+                           last_review_month_2015 = month(last_review_2015))
+
+listings_history <- mutate(listings_history, review_recency_2015_weeks = difftime("2016-01-01", last_review_2015, unit = "weeks"))
 
 
 
@@ -310,12 +305,14 @@ names(verification)[names(verification) == 'id'] <- 'listing_id'
 listings_history <- left_join(listings_history , amenities, by="listing_id")
 listings_history <- left_join(listings_history , verification, by="listing_id")
 
+listings_history <- mutate(listings_history,  has_reviews_2015 = num_reviews_in_2015 > 0, has_reviews_2016 = num_reviews_in_2016 > 0)
+
 
 listings_history <- listings_history[,c("host_id", "listing_id", "property_type", "room_type", "mean_price", "min_price", 
                                         "max_price", "is_multilisting", "host_since", "host_listings_count", "host_duration", "first_seen", 
                                         "last_seen", "first_seen_month", "last_seen_month",
                                         "listing_recency_2015_weeks", "scrap_duration", "total_occ_2015", "first_review", 
-                                        "first_review_year", "last_review", "last_review_year", "reviews_total_2015", "num_reviews_2015",           
+                                        "first_review_year", "last_review", "last_review_year", "num_as_of_2015", "num_reviews_in_2015",           
                                         "has_reviews_2015", "first_review_2015", "first_review_month_2015", "last_review_2015", 
                                         "last_review_month_2015", "review_recency_2015_weeks", "text_2015",
                                         "last_rating", "is_superhost_2015", "is_superhost_count_2015", "TV", "Internet", "Wireless.Internet", 
@@ -326,12 +323,12 @@ listings_history <- listings_history[,c("host_id", "listing_id", "property_type"
                                         "Cat.s.", "Indoor.Fireplace", "Suitable.for.Events", "Doorman", "Hot.Tub", "Elevator.in.Building", "Pool", 
                                         "Smoking.Allowed", "Other.pet.s.", "Washer...Dryer", "Lock.on.Bedroom.Door", "X24.Hour.Check.in", "Hangers", 
                                         "Hair.Dryer", "Iron", "Laptop.Friendly.Workspace", "translation.missing..en.hosting_amenity_49", 
-                                        "translation.missing..en.hosting_amenity_50", "email", "phone", "facebook", "reviews", 
+                                        "translation.missing..en.hosting_amenity_50","amenities_count", "email", "phone", "facebook", "reviews", 
                                         "kba", "google", "jumio", "sent_id", "linkedin", "manual_offline", "manual_online", "weibo", "photographer", 
                                         "None", "amex", "verifications_count", "total_occ_2016", "has_reviews_2016", 
                                         "first_review_2016", "last_review_2016", 
-                                        "reviews_total_2016", "num_reviews_2016", "text_2016", "is_superhost_2016", "is_superhost_count_2016" )]  
-listings_history1 <- mutate(listings_history, exist_in_2016 = (is.na(total_occ_2016)|is.na(has_reviews_2016)|is.na(num_reviews_2016)))
+                                        "num_as_of_2016", "num_reviews_in_2016", "text_2016", "is_superhost_2016", "is_superhost_count_2016" )]  
+listings_history <- mutate(listings_history, exist_in_2016 = (total_occ_2016 !=0))
 
 #set NAs in total_occ_2016 to zero
 #this means the listing no longer existed in 2016
@@ -339,20 +336,46 @@ listings_history[c("total_occ_2016")][is.na(listings_history[c("total_occ_2016")
 
 listings_history[c("text_2016", "text_2015")][is.na(listings_history[c("text_2016", "text_2015")])] <- ""
 
+#set to 0
+listings_history[c("num_reviews_in_2016", "num_reviews_in_2015")][is.na(listings_history[c("num_reviews_in_2016", "num_reviews_in_2015")])] <- 0
+listings_history[c("num_as_of_2016", "num_as_of_2015")][is.na(listings_history[c("num_as_of_2016", "num_as_of_2015")])] <- 0
+#
+listings_history[c("review_recency_2015_weeks")][is.na(listings_history[c("review_recency_2015_weeks")])] <- 53
+
+#
+listings_history[c("is_superhost_2015", "is_superhost_2016")][is.na(listings_history[c("is_superhost_2015", "is_superhost_2016")])] <- "f"
+
+
+
 #create a column that checks if listing still exist in 2016
-listings_history1 <- mutate(listings_history, exist_in_2016 = (is.na(total_occ_2016)|is.na(has_reviews_2016)|is.na(num_reviews_2016)))
+listings_history <- mutate(listings_history, exist_in_2016 = total_occ_2016 != 0)
+
+#set first and last review NAs to future date because it never happen
+listings_history <- mutate(listings_history, first_review_year = year(first_review), last_review_year = year(last_review))
+
+#property type undefined
+listings_history[c("property_type")][is.na(listings_history[c("property_type")])] <- "Undefined"
+
+listings_history[c("is_superhost_count_2016")][is.na(listings_history[c("is_superhost_count_2016")])] <- 0
+
+
+
+
+
 
 write_csv(listings_history, "listing_history.csv")
 
 x <- filter(all_listings, host_id == 2787) %>% select(listing_id, last_scraped, first_review, last_review, review_scores_rating, number_of_reviews)
-all_nas <- listings_history[is.na(listings_history$first_review),]
 
+all_nas <- listings_history[is.na(listings_history$first_review),]
 
 
 ######crime#####
 
 
 ####purge####
+
+listings_history <- read_csv("listing_history.csv")
 
 listings1511 <- read_csv("../../raw_data/2015-11-01-listings.csv")
 listings151120 <- read_csv("../../raw_data/2015-11-20-listings.csv") #watch out for this one! This is still November
