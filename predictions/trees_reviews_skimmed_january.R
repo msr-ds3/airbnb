97810398ce028b194df481ad309bec57d1ee640d
Jacqueline curran <jcurran.student@manhattan.edu>
2016-07-29 13:45:28 -0400

    January cohort trees reviews 2016

--- predictions/trees_reviews_skimmed_january.R
+++ predictions/trees_reviews_skimmed_january.R
@@ -178,7 +178,7 @@ tree_rv_p <- rpart(has_reviews_2016 ~ first_review_year + last_review_year +
                      first_review_month_2015 + last_review_month_2015 + 
                      review_recency_2015_weeks + last_rating + min_price + 
                      max_price + mean_price, 
-                   data = skimmed_train, 
+                   data = skimmed_jan_train, 
                    control = rpart.control(cp = 0.001))
 printcp(tree_rv_p)
 bestcp_rv_p<- tree_rv_p$cptable[which.min(tree_rv_p$cptable[,"xerror"]), "CP"]
@@ -191,14 +191,14 @@ text(tree_pruned_rv_p, cex = 0.8, use.n = TRUE, xpd = TRUE)
 prp(tree_pruned_rv_p, faclen = 0, cex = 0.8, extra = 1)
 
 #use the tree_pruned_rf to predict on the test set
-rv_p_predict <- predict(tree_pruned_rv_p, skimmed_test)
-ROCR_rv_p <- prediction(rv_p_predict, skimmed_test$has_reviews_2016)
+rv_p_predict <- predict(tree_pruned_rv_p, skimmed_jan_test)
+ROCR_rv_p <- prediction(rv_p_predict, skimmed_jan_test$has_reviews_2016)
 roc_perf_rv_p <- performance(ROCR_rv_p, measure = "tpr", x.measure = "fpr")
 plot(roc_perf_rv_p)
 abline(a=0, b= 1)
 auc_rv_p <- performance(ROCR_rv_p, measure = "auc")
 auc_rv_p@y.values
-#0.869
+#0.916
 
 ####################RECENCY, FREQUENCY, REVIEWS, AND PRICE####################
 #reviews ~ rf + rv + p
@@ -211,7 +211,7 @@ tree_rf_rv_p <- rpart(has_reviews_2016 ~ host_listings_count + host_duration +
                         num_as_of_2015 + num_reviews_in_2015 + has_reviews_2015 +
                         first_review_month_2015 + last_review_month_2015 + 
                         last_rating + mean_price + max_price + min_price, 
-                      data = skimmed_train, 
+                      data = skimmed_jan_train, 
                       control = rpart.control(cp = 0.001))
 printcp(tree_rf_rv_p)
 bestcp_rf_rv_p <- tree_rf_rv_p$cptable[which.min(tree_rf_rv_p$cptable[,"xerror"]), "CP"]
@@ -224,8 +224,8 @@ text(tree_pruned_rf_rv_p, cex = 0.8, use.n = TRUE, xpd = TRUE)
 prp(tree_pruned_rf_rv_p, faclen = 0, cex = 0.8, extra = 1)
 
 #use the tree_pruned_rf to predict on the test set
-rf_rv_p_predict <- predict(tree_pruned_rf_rv_p, skimmed_test)
-ROCR_rf_rv_p <- prediction(rf_rv_p_predict, skimmed_test$has_reviews_2016)
+rf_rv_p_predict <- predict(tree_pruned_rf_rv_p, skimmed_jan_test)
+ROCR_rf_rv_p <- prediction(rf_rv_p_predict, skimmed_jan_test$has_reviews_2016)
 roc_perf_rf_rv_p <- performance(ROCR_rf_rv_p, measure = "tpr", x.measure = "fpr")
 #plot the ROC curve
 plot(roc_perf_rf_rv_p)
@@ -233,7 +233,7 @@ abline(a=0, b= 1)
 #find the area under the roc curve
 auc_rf_rv_p <- performance(ROCR_rf_rv_p, measure = "auc")
 auc_rf_rv_p@y.values
-#0.908
+#0.935
 
 #############################AMENITIES TREE##################################
 #reviews ~ amen
@@ -251,7 +251,7 @@ tree_amen <- rpart(has_reviews_2016 ~  TV + Internet + Wireless.Internet +
                      Laptop.Friendly.Workspace + 
                      translation.missing..en.hosting_amenity_49 + 
                      translation.missing..en.hosting_amenity_50,
-                   data = skimmed_train, 
+                   data = skimmed_jan_train, 
                    control = rpart.control(cp = 0.001))
 printcp(tree_amen)
 bestcp_amen <- tree_amen$cptable[which.min(tree_amen$cptable[,"xerror"]), "CP"]
@@ -264,8 +264,8 @@ text(tree_pruned_amen, cex = 0.8, use.n = TRUE, xpd = TRUE)
 prp(tree_pruned_amen, faclen = 0, cex = 0.8, extra = 1)
 
 #use the tree_pruned_rf to predict on the test set
-predict_amen <- predict(tree_pruned_amen, skimmed_test)
-ROCR_amen <- prediction(predict_amen, skimmed_test$has_reviews_2016)
+predict_amen <- predict(tree_pruned_amen, skimmed_jan_test)
+ROCR_amen <- prediction(predict_amen, skimmed_jan_test$has_reviews_2016)
 roc_perf_amen <- performance(ROCR_amen, measure = "tpr", x.measure = "fpr")
 #plot the ROC curve
 plot(roc_perf_amen)
@@ -273,7 +273,7 @@ abline(a=0, b= 1)
 #find the area under the roc curve
 auc_amen <- performance(ROCR_amen, measure = "auc")
 auc_amen@y.values
-#0.795
+#0.767
 
 ###############AMMENITIES, RECENCY FREQUENCY, REVIEWS, PRICE####################
 #reviews ~ amen + rf + rv + p
@@ -300,7 +300,7 @@ tree <- rpart(has_reviews_2016 ~ TV + Internet + Wireless.Internet +
                 num_as_of_2015 + num_reviews_in_2015 + has_reviews_2015 +
                 first_review_month_2015 + last_review_month_2015 + 
                 last_rating + mean_price + max_price + min_price +room_type , 
-              data = skimmed_train, 
+              data = skimmed_jan_train, 
               control = rpart.control(cp = 0.001))
 printcp(tree)
 bestcp <- tree$cptable[which.min(tree$cptable[,"xerror"]), "CP"]
@@ -314,8 +314,8 @@ text(tree_pruned, cex = 0.8, use.n = TRUE, xpd = TRUE)
 tree_all <- prp(tree_pruned,faclen = 0, cex = 0.8, extra = 1)
 
 #use the tree_pruned_rf to predict on the test set
-predict <- predict(tree_pruned, skimmed_test)
-ROCR_tree <- prediction(predict, skimmed_test$has_reviews_2016)
+predict <- predict(tree_pruned, skimmed_jan_test)
+ROCR_tree <- prediction(predict, skimmed_jan_test$has_reviews_2016)
 roc_perf <- performance(ROCR_tree, measure = "tpr", x.measure = "fpr")
 #plot the ROC curve
 plot(roc_perf)
@@ -323,7 +323,7 @@ abline(a=0, b= 1)
 #find the area under the roc curve
 auc <- performance(ROCR_tree, measure = "auc")
 auc@y.values
-#0.9228
+#0.947
 
 ################VERIFICATION TREE#############################################
 tree_ver <- rpart(has_reviews_2016 ~  email + phone +
@@ -332,7 +332,7 @@ tree_ver <- rpart(has_reviews_2016 ~  email + phone +
                     manual_offline + manual_online + weibo + photographer + 
                     None +
                     amex + verifications_count,
-                  data = skimmed_train, 
+                  data = skimmed_jan_train, 
                   control = rpart.control(cp = 0.001))
 printcp(tree_ver)
 bestcp_ver <- tree_ver$cptable[which.min(tree_ver$cptable[,"xerror"]), "CP"]
@@ -345,8 +345,8 @@ text(tree_pruned_ver, cex = 0.8, use.n = TRUE, xpd = TRUE)
 prp(tree_pruned_ver, faclen = 0, cex = 0.8, extra = 1)
 
 #use the tree_pruned_rf to predict on the test set
-predict_ver <- predict(tree_pruned_ver, skimmed_test)
-ROCR_ver <- prediction(predict_ver, skimmed_test$has_reviews_2016)
+predict_ver <- predict(tree_pruned_ver, skimmed_jan_test)
+ROCR_ver <- prediction(predict_ver, skimmed_jan_test$has_reviews_2016)
 roc_perf_ver <- performance(ROCR_ver, measure = "tpr", x.measure = "fpr")
 #plot the ROC curve
 plot(roc_perf_ver)
@@ -354,7 +354,7 @@ abline(a=0, b= 1)
 #find the area under the roc curve
 auc_ver <- performance(ROCR_ver, measure = "auc")
 auc_ver@y.values
-#0.481
+#0.381
 
 ############RECENCY FREQUENCY, REVIEWS, AMMENITIES, VERIFICATION##############
 tree_all <- rpart(has_reviews_2016 ~  TV + Internet + Wireless.Internet +
@@ -386,7 +386,7 @@ tree_all <- rpart(has_reviews_2016 ~  TV + Internet + Wireless.Internet +
                     manual_offline + manual_online + weibo + photographer + 
                     None +
                     amex + verifications_count, 
-                  data = skimmed_train, 
+                  data = skimmed_jan_train, 
                   control = rpart.control(cp = 0.001))
 printcp(tree_all)
 bestcp_all <- tree_all$cptable[which.min(tree_all$cptable[,"xerror"]), "CP"]
@@ -399,8 +399,8 @@ text(tree_pruned_all, cex = 0.8, use.n = TRUE, xpd = TRUE)
 prp(tree_pruned_all, faclen = 0, cex = 0.8, extra = 1)
 
 #use the tree_pruned_rf to predict on the test set
-predict_all <- predict(tree_pruned_all, skimmed_test)
-ROCR_all <- prediction(predict_all, skimmed_test$has_reviews_2016)
+predict_all <- predict(tree_pruned_all, skimmed_jan_test)
+ROCR_all <- prediction(predict_all, skimmed_jan_test$has_reviews_2016)
 roc_perf_all <- performance(ROCR_all, measure = "tpr", x.measure = "fpr")
 #plot the ROC curve
 plot(roc_perf_all)
@@ -408,7 +408,7 @@ abline(a=0, b= 1)
 #find the area under the roc curve
 auc_all <- performance(ROCR_all, measure = "auc")
 auc_all@y.values
-#0.928
+#0.948
 
 #######################WORDS TREE#############################################
 #reviews ~ words
@@ -431,7 +431,7 @@ tree_word <- rpart(has_reviews_2016 ~ word_stay + word_apartment + word_great +
                      word_key + word_ny + word_central + word_small + word_muy + word_visit + 
                      word_building + word_es + word_excellent + word_appartement + word_metro + 
                      word_bedroom + word_trip + word_times + word_kind,                 
-                   data = skimmed_train, 
+                   data = skimmed_jan_train, 
                    control = rpart.control(cp = 0.001))
 printcp(tree_word)
 bestcp_word <- tree_word$cptable[which.min(tree_word$cptable[,"xerror"]), "CP"]
@@ -444,14 +444,14 @@ text(tree_word, cex = 0.8, use.n = TRUE, xpd = TRUE)
 prp(tree_pruned_word, faclen = 0, cex = 0.8, extra = 1)
 
 #use the tree_pruned_rf to predict on the test set
-word_predict <- predict(tree_pruned_word, skimmed_test)
-ROCR_word <- prediction(word_predict, skimmed_test$has_reviews_2016)
+word_predict <- predict(tree_pruned_word, skimmed_jan_test)
+ROCR_word <- prediction(word_predict, skimmed_jan_test$has_reviews_2016)
 roc_perf_word <- performance(ROCR_word, measure = "tpr", x.measure = "fpr")
 plot(roc_perf_word)
 abline(a=0, b= 1)
 auc_word <- performance(ROCR_word, measure = "auc")
 auc_word@y.values
-#0.801
+#0.866
 
 ########RECENCY FREQUENCY, REVIEWS, AMMENITIES, VERIFICATION & TEXT#############
 #reviews ~ rf + rv + amen + ver + text 
@@ -520,7 +520,7 @@ tree_all <- rpart(has_reviews_2016 ~  TV + Internet + Wireless.Internet +
                     word_building + word_es + word_excellent + 
                     word_appartement + word_metro + 
                     word_bedroom + word_trip + word_times + word_kind, 
-                  data = skimmed_train, 
+                  data = skimmed_jan_train, 
                   control = rpart.control(cp = 0.001))
 printcp(tree_all)
 bestcp_all <- tree_all$cptable[which.min(tree_all$cptable[,"xerror"]), "CP"]
@@ -533,8 +533,8 @@ text(tree_pruned_all, cex = 0.8, use.n = TRUE, xpd = TRUE)
 prp(tree_pruned_all, faclen = 0, cex = 0.8, extra = 1)
 
 #use the tree_pruned_rf to predict on the test set
-predict_all <- predict(tree_pruned_all, skimmed_test)
-ROCR_all <- prediction(predict_all, skimmed_test$has_reviews_2016)
+predict_all <- predict(tree_pruned_all, skimmed_jan_test)
+ROCR_all <- prediction(predict_all, skimmed_jan_test$has_reviews_2016)
 roc_perf_all <- performance(ROCR_all, measure = "tpr", x.measure = "fpr")
 #plot the ROC curve
 plot(roc_perf_all)
@@ -542,12 +542,12 @@ abline(a=0, b= 1)
 #find the area under the roc curve
 auc_all <- performance(ROCR_all, measure = "auc")
 auc_all@y.values
-#0.928
+#0.948
 
 ########################PURGED TREE############################################
 # reviews ~ purged
 tree_purged <- rpart(has_reviews_2016 ~ purged, 
-                     data = skimmed_train, 
+                     data = skimmed_jan_train, 
                      control = rpart.control(cp = 0.001))
 #view the tree
 printcp(tree_purged)
@@ -560,8 +560,8 @@ plot(tree_pruned_purged, uniform = TRUE)
 text(tree_pruned_purged, cex = 0.8, use.n = TRUE, xpd = TRUE)
 prp(tree_pruned_purged, faclen = 0, cex = 0.8, extra = 1)
 #use the tree_pruned_rf to predict on the test set
-purged_predict <- predict(tree_pruned_purged, skimmed_test)
-ROCR_purged <- prediction(purged_predict, skimmed_test$has_reviews_2016)
+purged_predict <- predict(tree_pruned_purged, skimmed_jan_test)
+ROCR_purged <- prediction(purged_predict, skimmed_jan_test$has_reviews_2016)
 roc.purg <- performance(ROCR_purged, measure = "tpr", x.measure = "fpr")
 #plot the ROC curve
 plot(roc.purg)
@@ -578,7 +578,7 @@ tree_location <- rpart(has_reviews_2016 ~ #neighbourhood_cleansed +
                          #city + 
                          zipcode + market + #country + 
                          is_location_exact,  
-                       data = skimmed_train, 
+                       data = skimmed_jan_train, 
                        control = rpart.control(cp = 0.001))
 #view the tree
 printcp(tree_location)
@@ -591,8 +591,8 @@ plot(tree_pruned_location, uniform = TRUE)
 text(tree_pruned_location, cex = 0.8, use.n = TRUE, xpd = TRUE)
 prp(tree_pruned_location, faclen = 0, cex = 0.8, extra = 1)
 #use the tree_pruned_rf to predict on the test set
-location_predict <- predict(tree_pruned_location, skimmed_test)
-ROCR_location <- prediction(location_predict, skimmed_test$has_reviews_2016)
+location_predict <- predict(tree_pruned_location, skimmed_jan_test)
+ROCR_location <- prediction(location_predict, skimmed_jan_test$has_reviews_2016)
 roc.loc <- performance(ROCR_location, measure = "tpr", x.measure = "fpr")
 #plot the ROC curve
 plot(roc.loc)
@@ -600,7 +600,7 @@ abline(a=0, b= 1)
 #find the area under the roc curve
 auc_location <- performance(ROCR_location, measure = "auc")
 auc_location@y.values
-#0.5
+#0.517
 
 ######################RF, RV, AMEN, VER, Location#############################
 # reviews ~ rf + rv + amen + location + ver
@@ -637,7 +637,7 @@ tree_all <- rpart(has_reviews_2016 ~  TV + Internet + Wireless.Internet +
                     #city + 
                     zipcode + market + #country + 
                     is_location_exact,  
-                  data = skimmed_train, 
+                  data = skimmed_jan_train, 
                   control = rpart.control(cp = 0.001))
 #view the tree
 printcp(tree_all)
@@ -650,8 +650,8 @@ plot(tree_pruned_all, uniform = TRUE)
 text(tree_pruned_all, cex = 0.8, use.n = TRUE, xpd = TRUE)
 prp(tree_pruned_all, faclen = 0, cex = 0.8, extra = 1)
 #use the tree_pruned_rf to predict on the test set
-all_predict <- predict(tree_pruned_all, skimmed_test)
-ROCR_all <- prediction(all_predict, skimmed_test$has_reviews_2016)
+all_predict <- predict(tree_pruned_all, skimmed_jan_test)
+ROCR_all <- prediction(all_predict, skimmed_jan_test$has_reviews_2016)
 roc_all <- performance(ROCR_all, measure = "tpr", x.measure = "fpr")
 #plot the ROC curve
 plot(roc_all)
@@ -659,7 +659,7 @@ abline(a=0, b= 1)
 #find the area under the roc curve
 auc_all <- performance(ROCR_all, measure = "auc")
 auc_all@y.values
-#0.928
+#0.948
 
 ###########AMMENITIES, VERIFICATION, WORDS, LOCATION, AND ROOM TYPE TREE######
 #REVIEWS ~ AMEN + VER + WORDS + LOCATION + ROOM TYPE
@@ -724,7 +724,7 @@ tree_non_rfrv <- rpart(has_reviews_2016 ~ min_price +
                          word_building + word_es + word_excellent + 
                          word_appartement + word_metro + 
                          word_bedroom + word_trip + word_times + word_kind, 
-                       data = skimmed_train, 
+                       data = skimmed_jan_train, 
                        control = rpart.control(cp = 0.01))
 printcp(tree_non_rfrv)
 bestcp_non<- tree_non_rfrv$cptable[which.min(tree_non_rfrv$cptable[,"xerror"]), "CP"]
@@ -737,11 +737,11 @@ text(tree_pruned_non, cex = 0.8, use.n = TRUE, xpd = TRUE)
 prp(tree_pruned_non, faclen = 0, cex = 0.8, extra = 1)
 
 #use the tree_pruned_rf to predict on the test set
-non_predict <- predict(tree_pruned_non, skimmed_test)
-ROCR_non <- prediction(non_predict, skimmed_test$has_reviews_2016)
+non_predict <- predict(tree_pruned_non, skimmed_jan_test)
+ROCR_non <- prediction(non_predict, skimmed_jan_test$has_reviews_2016)
 roc_perf_non <- performance(ROCR_non, measure = "tpr", x.measure = "fpr")
 plot(roc_perf_non)
 abline(a=0, b= 1)
 auc_non <- performance(ROCR_non, measure = "auc")
 auc_non@y.values
-#0.847
+#0.879
